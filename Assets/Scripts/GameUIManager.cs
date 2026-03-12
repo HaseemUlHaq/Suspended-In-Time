407f5558748e110041722b5b8843a9f46342bed1
Muhammad Naveed <naveed9630@gmail.com>
2026-03-12 13:06:40 +0100

    ui updates

---
+++ Assets/Scripts/GameUIManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameUIManager : MonoBehaviour
+{
+    public GameObject instructionPanel;
+    public GameObject startButton;
+    public GameObject pauseButton;
+    public GameObject pauseMenu;
+
+    void Start()
+    {
+        // Pause game at start
+        Time.timeScale = 0f;
+
+        instructionPanel.SetActive(true);
+        startButton.SetActive(true);
+
+        pauseButton.SetActive(false);
+        pauseMenu.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        Time.timeScale = 1f;
+
+        instructionPanel.SetActive(false);
+        startButton.SetActive(false);
+
+        pauseButton.SetActive(true);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+
+        pauseMenu.SetActive(true);
+        pauseButton.SetActive(false);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+
+        pauseMenu.SetActive(false);
+        pauseButton.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
